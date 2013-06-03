81ba33cad746d68e2bc72c07aee95425afd6a3a6
polamjag <indirectgeeks@gmail.com>
2013-06-03 10:39:55 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hello, coins! - source code for coins T-shirt
+
+## Usage
+* execute quine code    
+    $ ruby hello_coins_complete.rb
+* display source code itself (the result is same as above)
+    $ cat hello_coins_complete.rb
---
+++ hello_coins_complete.rb
@@ -0,0 +1,31 @@
+eval$s=%w'b="BAhsK3T/////////n/N/8MH///+c/59//v//5/z//PP//z
+/n  4ee  fP/j/OQc+//     yA/w8     Y4/nnY/x/wDzPPx/H/3MGeP7
+5P  P6f  M//zz8fx/5zxH3  8+zofnDP  jAAwMePOeADz4Y+PH///////
++f  ///  /////P/7//////  //5//8//  v////////H////4//+f////x
+39  ///  //3z    9+4MDg  Bx7w8QED  BD/gwM/     Pn+P5Oed/fv4
+8z  885  /PP      z43l+  zoOfnz/P  83P++P       /4MR6bc8/HB
+wz       4w   Jw   DHv7  g4Q/mHPj  5////   //   ///wM=";n=M
+ar       sh  al.l  oad(  b.unpack  ("m")   [0]   );e="eval$
+s=  %w"  <<        39<<  ($s*3);o  ="";j  =-1;   0.upto(30*
+60  -1)  {|  i|;o<<((n[  i]==1)?e  [j+=1   ]:3   2);o<<((i%
+59  ==5  8)   ?10:"");}   ;o[-10,  6]=""   <<3  9<<".    jo
+in  ";p  ut       s(o);      #b="      BA       hsK3     T/
+//  ///  ///       n/N/8     MH///     +c      /59//v   //5
+/z//PP//z/n4eefP/j/OQc+//yA/w8Y4/nnY/x/wDzPPx/H/3MGeP7  5PP
+6fM//zz8fx/5zxH38+zofnDPjAAwMePOeADz4Y+PH///////+f//   ////
+//P/7////////5//8//v////////H////4//+f////x39/////3z  9+4MD
+gBx7w8QEDBD/gwM/Pn+P5O   ed/fv48z885/PPz43l+zoOfnz/P83P++P/
+4MR6bc8/HBwz4wJwDHv7g4   Q/mHPj5/////////wM=";n=Marsh   al.
+load(b.unpack("m")[0]);  e="eval$s=%w"<<39<<($s*3);o=   "";
+j=-1;0 .upto(3 0*60-1){|i|;o<<((n[i]==1)?e[j +=1]:32)   ;o<
+<((      i%5      9=     =58)?1      0:""       );};o   [-1
+0,       6]        =     ""<<39       <<"      .join"  ;put
+s(  o);#b="  BAh   sK3T  //////  ///  n/N  /8MH///+c/  59//
+v/  /5/z//P  P//z  /n4e  efP/j/  OQc  +//    yA/w8Y4/  nnY/
+x/  wDzPPx   /H/3  MGeP  75PP6f  M//  zz8f     x/5zxH  38+z
+of  nDPjAAw  MePO  eADz  4Y+PH/  ///  ///+f//   //////P/7//
+//   ////5/   /8   //v/   // //  ///  H// //4/  /+f//   //x
+39       //       ///3z      9+  4MD  gBx       7w8Q    EDB
+D/gw     M/Pn    +P5Oed/     fv  48z  885      /PPz43  l+zo
+Ofnz/P83P++P/4MR6bc8/HBwz4wJwDHv7g4Q/mHPj5/////////wM=";n=M
+                    '.join 
